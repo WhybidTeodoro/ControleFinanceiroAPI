4e6ba01d1d8866355f42809659b36e93214e062a
WhybidTeodoro <twhybid@gmail.com>
2025-12-24 11:11:14 -0300

    Criado migrations da tabela User

---
+++ ControleFinanceiroAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiroAPI.Models;
+
+/// <summary>
+/// Representa o usuario do sistema.
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Identificador unico do usuario
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Email do usuario. Usado para login
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+    /// <summary>
+    /// Hash da senha do usuario. Usado para login
+    /// </summary>
+    public string PasswordHash { get; set; } = string.Empty;
+    /// <summary>
+    /// Data da criação do usuario. Automaticamente setado ao criar
+    /// </summary>
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
