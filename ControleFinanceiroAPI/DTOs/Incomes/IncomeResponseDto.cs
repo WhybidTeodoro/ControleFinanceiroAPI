315b74fea81a25f2befe97f760bac3e087a109e0
WhybidTeodoro <twhybid@gmail.com>
2025-12-25 11:16:09 -0300

    Criado Dto de response para Income

---
+++ ControleFinanceiroAPI/DTOs/Incomes/IncomeResponseDto.cs
@@ -0,0 +1,27 @@
+﻿namespace ControleFinanceiroAPI.DTOs.Incomes;
+
+/// <summary>
+/// Dto responsavel por retornar os dados nos endpoints de Income
+/// </summary>
+public class IncomeResponseDto
+{
+    /// <summary>
+    /// Identificador unico do usuario no dto reponse
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Representa a renda do usuario no dto response
+    /// </summary>
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Representa a data em que foi adicionada a renda do usuario no dto response
+    /// </summary>
+    public DateTime Data { get; set; }
+
+    /// <summary>
+    /// Representa a descrição da renda do usuario no dto response
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}
