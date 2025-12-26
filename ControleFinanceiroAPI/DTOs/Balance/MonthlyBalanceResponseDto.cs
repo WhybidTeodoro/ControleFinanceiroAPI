1054e5f64338e43dd899c8d5dff02fb474d97d3e
WhybidTeodoro <twhybid@gmail.com>
2025-12-26 15:14:35 -0300

    Criado Dto de resposta para saldo mensal

---
+++ ControleFinanceiroAPI/DTOs/Balance/MonthlyBalanceResponseDto.cs
@@ -0,0 +1,32 @@
+﻿namespace ControleFinanceiroAPI.DTOs.Monthl;
+
+/// <summary>
+/// Dto responsavel por retornar o resumo financeiro mensal do usuario
+/// </summary>
+public class MonthlyBalanceResponseDto
+{
+    /// <summary>
+    /// Mês de referencia do saldo
+    /// </summary>
+    public int Month { get; set; }
+
+    /// <summary>
+    /// Ano de referencia do saldo
+    /// </summary>
+    public int Year { get; set; }
+
+    /// <summary>
+    /// Total de receita do mês
+    /// </summary>
+    public decimal TotalRecipe { get; set; }
+
+    /// <summary>
+    /// Total de despesas do mês
+    /// </summary>
+    public decimal TotalExpense { get; set; }
+
+    /// <summary>
+    /// Saldo final do mês (Receitas - Despesas)
+    /// </summary>
+    public decimal Balance { get; set; }
+}
